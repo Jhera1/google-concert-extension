05f1df62d1341852922a9f6f5bb9131f782e9493
Justin Armstrong <armstrongjustin034@gmail.com>
2024-10-09 20:54:15 -0700

    added js.script file

---
+++ script.js
@@ -0,0 +1,33 @@
+const http = new XMLHttpRequest()
+let result = document.querySelector("#result")
+
+document.querySelector("#share").addEventListener
+("click", () => {
+    findMyCoordinates()
+})
+function findMyCoordinates() {
+     if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition
+        ((position) => {
+            const bdcAPI = `https://api-bdc.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&`
+            getAPI(bdcAPI)
+        },
+        (err) => {
+           alert(err.message)
+        })
+     } else {
+        alert("Geolocation is not supported by you're browser")
+     }
+}
+
+function getAPI(bdcAPI) {
+    http.open("GET", bdcAPI)
+    http.send()
+    http.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status ==200) {
+            result.innerHTML = this.responseText
+            const results = JSON.parse(this.responseText)
+            console.log(results.locality)
+        }
+    }
+}
