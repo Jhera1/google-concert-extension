0c0551bdc3a1d505f474e76944ffcc1b3ec3eaf1
Jonathan Ruiz <joanthan.ruiz.1793@gmail.com>
2024-10-12 00:51:26 -0500

    Added function to calculate dates
    this function calculates todays date and the date two weeks from now.
    so every time you click the extension you get the info for the correct
    dates.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const http = new XMLHttpRequest();
-let result = document.querySelector("#result");
 import { config } from "./config";
 
+const getDates = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const twoWeeksLater = new Date(today);
+  twoWeeksLater.setDate(today.getDate() + 14);
+  const yearInTwoWeeks = twoWeeksLater.getFullYear();
+  const monthInTwoWeeks = twoWeeksLater.getMonth() + 1;
+  const dayInTwoWeeks = twoWeeksLater.getDate();
+  return [
+    [year, month, day],
+    [yearInTwoWeeks, monthInTwoWeeks, dayInTwoWeeks],
+  ];
+};
+
 const getConcertData = async (cityName) => {
-  console.log(cityName);
-  const url = `https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${cityName}&minDate=2024-10-01&maxDate=2024-10-18&page=1`;
+  const date = getDates();
+  const todaysDate = date[0].join("-");
+  const endDate = date[1].join("-");
+  const url = `https://concerts-artists-events-tracker.p.rapidapi.com/location?name=${cityName}&minDate=${todaysDate}&maxDate=${endDate}&page=1`;
   const options = {
     method: "GET",
     headers: {
@@ -17,12 +34,9 @@ const getConcertData = async (cityName) => {
     const response = await fetch(url, options);
     const result = await response.json();
     const concertList = document.getElementById("concerts");
-    console.log(result);
     concertList.innerHTML = result.data
       .map((concert) => `<li>${concert.name} DATE: ${concert.endDate} </li> `)
       .join("");
-
-    console.log(result.data[10]);
   } catch (error) {
     console.error(error);
   }
@@ -37,7 +51,6 @@ function findMyCoordinates() {
       (position) => {
         const bdcAPI = `https://api-bdc.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&`;
         getAPI(bdcAPI);
-        console.log(bdcAPI);
         return bdcAPI;
       },
       (err) => {
@@ -55,7 +68,6 @@ function getAPI(bdcAPI) {
   http.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const results = JSON.parse(this.responseText);
-      console.log(results.locality);
       getConcertData(results.locality);
       return results.locality;
     }
